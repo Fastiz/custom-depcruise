674cb0745048abcde6135490c95532effce4e8de
fastiz <fastiz@itba.edu.ar>
2023-03-18 17:29:00 +0100

    caching nodes

--- src/service/dependencytree/DependencyTreeServiceImpl.ts
+++ src/service/dependencytree/DependencyTreeServiceImpl.ts
@@ -2,6 +2,8 @@ import { type DependencyTreeNode } from 'src/model/DependencyTreeNode'
 import { type SourceFile } from 'src/model/File'
 import { type FileRepository } from 'src/repository/FileRepository'
 import { type DependencyTreeService } from 'src/service/dependencytree/DependencyTreeService'
+import { notUndefined } from '../../util/notUndefined'
+import { ImportDependency } from '../../model/ImportDependency'
 
 export class DependencyTreeServiceImpl implements DependencyTreeService {
   readonly fileRepository: FileRepository
@@ -12,13 +14,27 @@ export class DependencyTreeServiceImpl implements DependencyTreeService {
 
   buildDependencyTreeFromFilePath = async (rootPath: string): Promise<DependencyTreeNode> => {
     const sourceFile: SourceFile = { path: rootPath }
-    return await this.buildDependencyTreeFromRootPathRec(sourceFile)
+    const cachedNodes = new Map<string, DependencyTreeNode>()
+    return await this.buildDependencyTreeFromRootPathRec(sourceFile, cachedNodes)
   }
 
-  buildDependencyTreeFromRootPathRec = async (sourceFile: SourceFile): Promise<DependencyTreeNode> => {
+  buildDependencyTreeFromRootPathRec = async (sourceFile: SourceFile, cachedNodes: Map<string, DependencyTreeNode>): Promise<DependencyTreeNode> => {
     const imports = await this.fileRepository.readImportsFromSourceFile(sourceFile) ?? null
-    const dependencies = await Promise.all(imports
-      .map(async ({ to }) => await this.buildDependencyTreeFromRootPathRec(to)))
+
+    const getDependencyNode = async ({ to }: ImportDependency) => {
+      const cachedNode = cachedNodes.get(to.path)
+
+      if (notUndefined(cachedNode)) {
+        return cachedNode
+      }
+
+      const newNode = await this.buildDependencyTreeFromRootPathRec(to, cachedNodes)
+      cachedNodes.set(to.path, newNode)
+
+      return newNode
+    }
+
+    const dependencies = await Promise.all(imports.map(getDependencyNode))
 
     return {
       nodeFile: sourceFile,
